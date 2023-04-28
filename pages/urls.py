d358a0edc1355ec53342ec9470a9cd13eac987f5
Garanaurt <bnotchenko@gmail.com>
2023-04-28 13:46:19 +0300

    show all posts on main page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+ 
+from .views import BlogListView
  
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', BlogListView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import ListView
+ 
 from .models import Post
  
-class HomePageView(ListView):
+ 
+class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
-    context_object_name = 'all_posts_list'
-# Create your views here.
