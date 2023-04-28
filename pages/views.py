95aa2bb5683ccefdef1d83f0aa4161eff237ee0c
Garanaurt <bnotchenko@gmail.com>
2023-04-28 12:02:38 +0300

    home page show str from db

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
  
- 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name = 'all_posts_list'
 # Create your views here.
-
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
